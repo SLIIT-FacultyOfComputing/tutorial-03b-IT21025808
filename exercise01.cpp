51ecbe14b139ab8032e4972de00634dd1c8b2372
IT21025808 <87629957+IT21025808@users.noreply.github.com>
2021-08-04 08:37:51 +0000

    Completed

--- exercise01.cpp
+++ exercise01.cpp
@@ -1,12 +1,23 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
-int main() {
- float marks[] = {78.4, 90.6, 45.9, 72.2, 54.4};
- char names[][20] = {"Ajith", "Wimal", "Kanthi", "Suranji", "Kushmitha"};
- cout << "No" << "Name" << "Marks" << endl;
- for (int r = 0; r < 5; r++) {
-     cout <<  r+1 
-          <<  names[r]
-          << marks[r] << endl;
- }
+
+int main()
+{
+
+	float marks[10] = {78.4, 90.6, 45.9, 72.2, 54.4};
+	char name[5][120] = {"Ajith","Wimal","Kanthi","Suranji","Kushmitha"};
+	
+	cout << setw(5)<< "No" << setw(15) <<  "Name" <<setw(10) <<  "Marks" << endl;
+	
+	
+	for(int r=0; r <5 ; r++)
+	{
+		cout <<setw(5) << r+1;
+		cout << setw(15) << name[r];
+		cout << setw(10) << fixed << showpoint << setprecision(2)<< marks[r];
+		cout << endl;
+		
+	}
+	return 0;
 }
--- exercise04.cpp
+++ exercise04.cpp
@@ -1,21 +1,29 @@
-#include <iostream>
+#include<iostream>
+#include<iomanip>
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int length, int width);
 
-// Do not change the main() function
-int main() {
-   int length = 10, width = 5;
-   input(length, width);
-   print(length, width);
-   return 0;
+
+int main()
+{
+	int length = 10;
+	int width = 5;
+	print(length,width);
+	input(length,width);
+	
+	return 0;
 }
 
-// Do not change the print() function
-void print(int len, int wth) {
-   cout << "Length : " << len 
-        << ", Width  : " << wth << endl;
+void print(int len,int wth)
+{
+	cout << "Length :" << len << endl;
+	cout << "Width :" << wth  << endl ;
 }
 
-// Implement the Input Function here
+void input(int length,int width)
+{
+	cout << "Length :" << &length << endl;
+	cout << "Width :" << &width  << endl;
+}
